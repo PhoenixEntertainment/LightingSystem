06f66acc59efb1d03e6a791556ce799bb38fefc0
Noble_Draconian <Binarybyte.DEV@gmail.com>
2023-03-14 17:19:25 -0400

    Add initial system

--- Client/src/init.lua
+++ Client/src/init.lua
@@ -1,95 +1,89 @@
 --[[
-	Example Controller
-	This is an example Controller to display how a Controller is created and how it should be formatted.
+	Lighting controller
+	Handles the loading and saving of different game lighting states
 --]]
 
-local ExampleController={}
+local LightingController = {}
+
+---------------------
+-- Roblox Services --
+---------------------
+local Lighting = game:GetService("Lighting")
 
 ------------------
 -- Dependencies --
 ------------------
-local ExampleService;
+local BaseLighting = require(script.BaseLighting)
 
 -------------
--- DEFINES --
+-- Defines --
 -------------
-local Connections={} --Holds all event connections to allow for state cleanup if the controller is stopped.
-local ControllerRunning = false --Used to determine when the controller is stopped
-local Generation_Interval = 3 --The interval (in seconds) at which to generate new GUIDs.
+local LightingStates = {}
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- @Name : GetGUID
--- @Description : This is an example controller method.
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function ExampleController:GetGUID()
-	self:Log("[Example Controller] Getting new GUID from server.")
-
-	return self.Services.ExampleService:GetGUID()
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- @Name : RegisterLightingState
+-- @Description : Registers the given lighting state that can be loaded later
+-- @Params : string "StateName" - The name to reference the lighting state by
+--           table "LightingState" - A table containing the lighting state to register
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function LightingController:RegisterLightingState(StateName,LightingState)
+	if LightingStates[StateName] == nil then
+		LightingStates[StateName] = LightingState
+		self:DebugLog("[Lighting Controller] Registered lighting state '"..StateName.."'.")
+	else
+		self:Log(
+			("[Lighting Controller] RegisterLightingState() : Could not register lighting state '%s', a lighting state with that name already exists!")
+			:format(StateName),
+			"Error"
+		)			
+	end
 end
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- @Name : LoadLightingState
+-- @Description : Loads the specified lighting state
+-- @Params : string "StateName" - The name of the lighting state to load
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function LightingController:LoadLightingState(StateName)
+	if LightingStates[StateName] ~= nil then
+		local LightingState = LightingStates[StateName]
+
+		Lighting:ClearAllChildren()	
+		for LightingProperty_Name,LightingProprty_Value in pairs(LightingState.Properties) do
+			Lighting[LightingProperty_Name] = LightingProprty_Value
+		end
+		for _,LightingEffect in pairs(LightingState.Effects) do
+			LightingEffect:Clone().Parent = Lighting
+		end
+	else
+		self:Log(
+			("[Lighting Controller] LoadLightingState() : Could not load lighting state %s, no lighting state with that name has been registered!")
+			:format(StateName),
+			"Error"
+		)
+	end
+end
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- @Name : Init
--- @Description : Called when the Controller module is first loaded.
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function ExampleController:Init()
-	ExampleService = self:GetService("ExampleService")
+-- @Description : Used to initialize controller state
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function LightingController:Init()
+	self:DebugLog("[Lighting Controller] Initializing...")
 
-	self:DebugLog("[Example Controller] Initialized!")
+	self:RegisterLightingState("Default",BaseLighting)
+	self:LoadLightingState("Default")
+
+	self:DebugLog("[Lighting Controller] Initialized!")
 end
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- @Name : Start
--- @Description : Called after all Controllers are loaded.
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function ExampleController:Start()
-	self:DebugLog("[Example Controller] Started!")
-	ControllerRunning = true
+-- @Description : Used to run the controller
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function LightingController:Start()
+	self:DebugLog("[Lighting Controller] Running!")
 
-	--------------------------------------------
-	-- Listen for GUID creation on the server --
-	--------------------------------------------
-	local GUIDListener = ExampleService.GUIDGenerated:connect(function(GUID)
-		self:Log("[Example Controller] Example Service just generated the GUID '" .. GUID .. "'.")
-	end)
-	table.insert(Connections,GUIDListener)
-
-	-------------------------------------------
-	-- Generationg a new GUID at an interval --
-	-------------------------------------------
-	while ControllerRunning do
-		task.wait(Generation_Interval)
-		local GUID = ExampleService:GetGUID()
-		self:Log("[Example Controller] New GUID is '"..GUID.."'.")
-	end
 end
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- @Name : Stop
--- @Description : Called when the controller is being stopped.
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function ExampleController:Stop()
-
-	ControllerRunning = false
-
-	-------------------------------------------
-	-- Disconnect and remove all connections --
-	-------------------------------------------
-	for Index = 1,#Connections do
-		Connections[1]:Disconnect()
-		table.remove(Connections,Index)
-	end
-
-	self:Log("[Example Controller] Stopped!")
-end
-
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- @Name : Unload
--- @Description : Called when the controller is being unloaded.
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function ExampleController:Unload()
-
-	self:Log("[Example Controller] Unloaded!")
-end
-
-
-return ExampleController
+return LightingController
